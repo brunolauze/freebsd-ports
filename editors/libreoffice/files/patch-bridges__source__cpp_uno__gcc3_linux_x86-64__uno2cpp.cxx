a3163919088befa30dbc8f8700b2f1d9576a21ee
Bruno Lauze <brunolauze@msn.com>
2013-12-24 12:27:03 -0600

    Upgrading libreoffice to 4.2.0.1

--- editors/libreoffice/files/patch-bridges__source__cpp_uno__gcc3_linux_x86-64__uno2cpp.cxx
+++ editors/libreoffice/files/patch-bridges__source__cpp_uno__gcc3_linux_x86-64__uno2cpp.cxx
@@ -1,14 +1,20 @@
---- bridges/source/cpp_uno/gcc3_linux_x86-64/uno2cpp.cxx.orig	2013-10-15 10:12:53.959354972 -0500
-+++ bridges/source/cpp_uno/gcc3_linux_x86-64/uno2cpp.cxx	2013-10-15 10:13:54.579352510 -0500
-@@ -290,7 +290,11 @@
+--- bridges/source/cpp_uno/gcc3_linux_x86-64/uno2cpp.cxx.orig	2013-12-24 12:19:12.919113849 -0600
++++ bridges/source/cpp_uno/gcc3_linux_x86-64/uno2cpp.cxx	2013-12-24 12:19:48.720589745 -0600
+@@ -289,11 +289,12 @@
+     }
       catch (...)
       {
-           // fill uno exception
-+	#ifdef _LIBCPP_VERSION
-+        CPPU_CURRENT_NAMESPACE::fillUnoException( __cxxabiv1::__cxa_get_globals()->caughtExceptions, *ppUnoExc, pThis->getBridge()->getCpp2Uno() );
-+	#else
-         fillUnoException( __cxa_get_globals()->caughtExceptions, *ppUnoExc, pThis->getBridge()->getCpp2Uno() );
-+	#endif
+-         // fill uno exception
+-         fillUnoException(
+-             reinterpret_cast< CPPU_CURRENT_NAMESPACE::__cxa_eh_globals * >(
+-                 __cxxabiv1::__cxa_get_globals())->caughtExceptions,
+-             *ppUnoExc, pThis->getBridge()->getCpp2Uno());
++
++        #ifdef _LIBCPP_VERSION
++         CPPU_CURRENT_NAMESPACE::fillUnoException( __cxxabiv1::__cxa_get_globals()->caughtExceptions, *ppUnoExc, pThis->getBridge()->getCpp2Uno() );
++        #else
++          fillUnoException( __cxa_get_globals()->caughtExceptions, *ppUnoExc, pThis->getBridge()->getCpp2Uno() );
++        #endif
  
          // temporary params
-         for ( ; nTempIndizes--; )
+         for ( ; nTempIndices--; )
--- editors/libreoffice/files/patch-slideshow_source_engine_activities_activitiesfactory.cxx
+++ editors/libreoffice/files/patch-slideshow_source_engine_activities_activitiesfactory.cxx
@@ -1,5 +1,5 @@
---- slideshow/source/engine/activities/activitiesfactory.cxx.orig	2013-10-14 18:03:58.383361385 -0500
-+++ slideshow/source/engine/activities/activitiesfactory.cxx	2013-10-14 18:06:42.096351667 -0500
+--- slideshow/source/engine/activities/activitiesfactory.cxx.orig	2013-12-24 12:05:06.999305060 -0600
++++ slideshow/source/engine/activities/activitiesfactory.cxx	2013-12-24 12:06:18.750478679 -0600
 @@ -298,7 +298,7 @@
          if( mbCumulative && !mbDynamicStartValue )
          {
@@ -18,21 +18,3 @@
                              lerp( maInterpolator,
                                    (mbDynamicStartValue
                                     ? mpAnim->getUnderlyingValue()
-@@ -557,7 +557,7 @@
-         // interpolate between nIndex and nIndex+1 values
-         (*mpAnim)(
-             getPresentationValue(
--                accumulate( maValues.back(),
-+                accumulate<ValueType>( maValues.back(),
-                             mbCumulative ? nRepeatCount : 0,
-                             maInterpolator( maValues[ nIndex ],
-                                             maValues[ nIndex+1 ],
-@@ -577,7 +577,7 @@
-         // this is discrete, thus no lerp here.
-         (*mpAnim)(
-             getPresentationValue(
--                accumulate( maValues.back(),
-+                accumulate<ValueType>( maValues.back(),
-                             mbCumulative ? nRepeatCount : 0,
-                             maValues[ nFrame ] ) ) );
-     }
