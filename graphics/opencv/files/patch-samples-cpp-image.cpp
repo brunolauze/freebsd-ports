a148810dfef92329145cc90f33c9f89fdfe2447b
Bruno Lauze <brunolauze@msn.com>
2013-12-25 22:57:48 -0600

    Update make.conf

--- graphics/opencv/files/patch-samples-cpp-image.cpp
+++
@@ -1,7 +0,0 @@
---- samples/cpp/image.cpp.orig	2013-10-07 22:00:15.757797540 -0500
-+++ samples/cpp/image.cpp	2013-10-07 22:00:28.920929588 -0500
-@@ -1,3 +1,4 @@
-+#include <cstdio>
- #include <stdio.h>
- #include <iostream>
- #include "opencv2/imgproc/imgproc.hpp"
