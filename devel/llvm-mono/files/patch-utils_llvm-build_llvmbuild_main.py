2d864dc3a280b26e58141240590913de8b02e2c5
Bruno Lauze <brunolauze@msn.com>
2014-01-14 08:15:57 -0600

    Updating failing ports

--- devel/llvm-mono/files/patch-utils_llvm-build_llvmbuild_main.py
+++ devel/llvm-mono/files/patch-utils_llvm-build_llvmbuild_main.py
@@ -1,20 +1,18 @@
-
-$FreeBSD: devel/llvm33/files/patch-utils_llvm-build_llvmbuild_main.py 300896 2012-07-14 13:54:48Z beat $
-
---- utils/llvm-build/llvmbuild/main.py.orig
-+++ utils/llvm-build/llvmbuild/main.py
-@@ -633,7 +633,13 @@
+--- utils/llvm-build/llvmbuild/main.py.orig	2014-01-14 08:12:12.456807590 -0600
++++ utils/llvm-build/llvmbuild/main.py	2014-01-14 08:12:57.506029478 -0600
+@@ -660,9 +660,12 @@
  
      # We handle a few special cases of target names here for historical
      # reasons, as these are the names configure currently comes up with.
 -    native_target_name = { 'x86' : 'X86',
+-                           'x86_64' : 'X86',
+-                           'Unknown' : None }.get(opts.native_target,
 +    native_target_name = { 'amd64' : 'X86',
-+			   'arm' : 'ARM',
-+			   'i386' : 'X86',
-+			   'mips' : 'Mips',
-+			   'powerpc' : 'PowerPC',
-+			   'sparc64' : 'Sparc',
-+                           'x86' : 'X86',
-                            'x86_64' : 'X86',
-                            'Unknown' : None }.get(opts.native_target,
++                          'arm' : 'ARM',
++                          'i386' : 'X86',
++                          'x86' : 'X86',
++                          'x86_64' : 'X86',
++                          'Unknown' : None }.get(opts.native_target,
                                                    opts.native_target)
+     if native_target_name is None:
+         native_target = None
