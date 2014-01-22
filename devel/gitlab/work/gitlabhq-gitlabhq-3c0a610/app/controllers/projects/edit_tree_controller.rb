eace90234cf538b7b224ba0001559fad058c5c95
Bruno Lauze <brunolauze@msn.com>
2014-01-22 13:31:14 -0600

    Fixing mono

---
+++ devel/gitlab/work/gitlabhq-gitlabhq-3c0a610/app/controllers/projects/edit_tree_controller.rb
@@ -0,0 +1,26 @@
+class Projects::EditTreeController < Projects::BaseTreeController
+  before_filter :require_branch_head
+  before_filter :blob
+
+  def show
+    @last_commit = Gitlab::Git::Commit.last_for_path(@repository, @ref, @path).sha
+  end
+
+  def update
+    result = Files::UpdateService.new(@project, current_user, params, @ref, @path).execute
+
+    if result[:status] == :success
+      flash[:notice] = "Your changes have been successfully committed"
+      redirect_to project_blob_path(@project, @id)
+    else
+      flash[:alert] = result[:error]
+      render :show
+    end
+  end
+
+  private
+
+  def blob
+    @blob ||= @repository.blob_at(@commit.id, @path)
+  end
+end
