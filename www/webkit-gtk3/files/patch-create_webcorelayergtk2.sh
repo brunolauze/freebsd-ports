e7fea7aea3c2db939e7300ebfcf09777027f0f98
Bruno Lauze <brunolauze@msn.com>
2014-01-24 09:09:29 -0600

    Update ports distinfo ; fixing cef port

--- www/webkit-gtk3/files/patch-create_webcorelayergtk2.sh
+++ www/webkit-gtk3/files/patch-create_webcorelayergtk2.sh
@@ -1,1 +1,13 @@
-Hmm...  I can't seem to find a patch in there anywhere.
+--- /dev/null	2014-01-24 06:55:00.000000000 -0600
++++ create_webcorelayergtk2.sh	2014-01-24 06:56:26.525471780 -0600
+@@ -0,0 +1,10 @@
++#!/bin/sh
++
++find . -name "libPlatform_la-*.o" -exec ar r WebCoreLayerGtk2.a {} +
++find . -name "libPlatformGtk2_la-*.o" -exec ar r WebCoreLayerGtk2.a {} +
++find . -name "libWebCorePlatform_la-*.o" -exec ar r WebCoreLayerGtk2.a {} +
++find . -name "libWebCoreGtk2_la-*.o" -exec ar r WebCoreLayerGtk2.a {} +
++find . -name "libWebCore_la-*.o" -exec ar r WebCoreLayerGtk2.a {} +
++find . -name "libWebCoreModules_la-*.o" -exec ar r WebCoreLayerGtk2.a {} +
++find . -name "libWebCoreSVG_la-*.o" -exec ar r WebCoreLayerGtk2.a {} +
++find . -name "libANGLE_la-*" -exec ar r WebCoreLayerGtk2.a {} +
